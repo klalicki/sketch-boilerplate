7d47dda0daf933f0f1049711a4c72a983b8b3c22
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2023-05-21 19:57:19 +0000

    adds sass loader, currently imported via script

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ const config = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "index.html",
+      template: "src/index.html",
     }),
 
     // Add your plugins here
@@ -35,8 +35,8 @@ const config = {
         use: [stylesHandler, "css-loader"],
       },
       {
-        test: /\.s[ac]ss$/i,
-        use: [stylesHandler, "css-loader", "sass-loader"],
+        test: /\.(scss|css)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
